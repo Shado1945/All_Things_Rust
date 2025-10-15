bf7d1001d22375084847ab28d9fdac4e93dcb1c7
Shado1945 <viljoendev777@gmail.com>
2025-10-15 23:01:12 +0200

    [ADD] Added the fibonaci code

--- fibonaci_sequence/src/main.rs
+++ fibonaci_sequence/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    // // Unoptimise
+    // println!("This is to get the nth place of the fibonaci sequence");
+    // let n = 6;
+    // let mut second = 0;
+    // let mut result = 1;
+    // for fib in 0..n {
+    //     println!("{} Fibonaci number = {}", fib + 1, result);
+    //     if second == 0 {
+    //         second = second + 1;
+    //     } else {
+    //         result = result + second;
+    //         second = result - second;
+    //     }
+    // }
+
+    // // optimised
+    println!("This is to get the nth place of the fibonaci sequence");
+    let n = 6;
+    let (mut second, mut result) = (0, 1);
+    for fib in 1..=n {
+        println!("{} Fibonaci number = {}", fib, result);
+        let temp = second + result;
+        second = result;
+        result = temp;
+    }
 }
